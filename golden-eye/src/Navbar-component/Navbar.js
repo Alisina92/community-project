98174579f2347cd1031a94d051aa4063bd4e1666
[Alisina92] <asb92.jobs@gmail.com>
2020-11-29 11:31:20 +0000

    Navbar Done

---
+++ golden-eye/src/Navbar-component/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Navbar = () => {
+  const Navbar = styled.nav`
+    background: black;
+    padding: 0.1rem;
+    width: 100%;
+  `;
+  const Ul = styled.ul`
+    list-style: none;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  `;
+  
+  const List = styled.li`
+    color: white;
+  `;
+  return (
+    <Navbar>
+      <Ul>
+        <Link to="/">
+          <List>Home</List>
+        </Link>
+        <Link to="/learning">
+          <List>Learning Material</List>
+        </Link>
+        <Link to="/slack">
+          <List>Slack Community</List>
+        </Link>
+        <Link to="/projects">
+          <List>Projects</List>
+        </Link>
+        <Link to="/contact">
+          <List>Contact</List>
+        </Link>
+        <Link to="/Login">
+          <List>Login</List>
+        </Link>
+      </Ul>
+    </Navbar>
+  );
+};
+export default Navbar;
