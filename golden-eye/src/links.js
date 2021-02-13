786767478f18d7cbbca5b4d8bfd5f7da449a01f2
Sonjide Hussain <sonjide.hussain@gmail.com>
2021-02-13 15:36:33 +0000

    added links file

---
+++ golden-eye/src/links.js
@@ -0,0 +1,9 @@
+const links =[
+    {id:"1",
+    webName:"freeCodeCamp",
+    URL:"https://www.freecodecamp.org/"},
+    {id:"2",
+    webName:"Khan Academy",
+    URL:"https://www.khanacademy.org/"}
+];
+
